41f08ea88bc1d6d5df61a9ba0abef9fe95655509
doDomoGu <kaka_71936410@qq.com>
2016-12-13 16:52:19 +0800

    login 登录  和  oa模块

--- common/models/User.php
+++ common/models/User.php
@@ -47,36 +47,8 @@ class User extends \yii\db\ActiveRecord
         ];
     }
 
-/*CREATE TABLE `user` (
-`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-`username` varchar(100) NOT NULL,
-`password` varchar(100) NOT NULL,
-`reg_code` varchar(255) NOT NULL,
-`forgetpw_code` varchar(255) NOT NULL,
-`name` varchar(100) NOT NULL,
-`is_admin` tinyint(1) unsigned DEFAULT '0',
-`position_id` int(9) unsigned NOT NULL DEFAULT '0',
-`gender` tinyint(1) unsigned NOT NULL DEFAULT '0',
-`birthday` date DEFAULT NULL,
-`join_date` date DEFAULT NULL,
-`mobile` varchar(255) NOT NULL DEFAULT '',
-`phone` varchar(255) NOT NULL DEFAULT '',
-`describe` text NOT NULL,
-`ord` int(9) NOT NULL DEFAULT '0',
-`status` tinyint(1) NOT NULL DEFAULT '0',
-PRIMARY KEY (`id`),
-UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8*/
-
-
-    /*ALTER TABLE `user`
-     ADD `contract_date` DATE DEFAULT NULL AFTER `join_date`,
-     ADD `head_img` VARCHAR(255) DEFAULT NULL AFTER `contract_date`;*/
-
-    /*ALTER TABLE `user` ADD `password_true` VARCHAR(255) DEFAULT NULL AFTER `password`;*/
-
-    /*public function getPosition()
+    /*public static function findByUsername($username)
     {
-        return $this->hasOne('app\models\Position', array('id' => 'position_id'));
+        return User::findOne(['username' => $username, 'status' => 1]);
     }*/
 }
