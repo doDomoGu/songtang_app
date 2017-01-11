afee84b14ee0a9392e56d75b653b7592321fe5b2
doDomoGu <kaka_71936410@qq.com>
2017-01-12 02:47:47 +0800

    api 返回 json格式

--- api/modules/ucenter/controllers/UserController.php
+++ api/modules/ucenter/controllers/UserController.php
@@ -4,12 +4,20 @@ namespace api\modules\ucenter\controllers;
 
 use ucenter\models\User;
 use yii\rest\ActiveController;
+use yii\web\Response;
 
 class UserController extends ActiveController
 {
     public function init(){
         $this->modelClass = User::className();
         parent::init();
+    }
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['contentNegotiator']['formats'] = ['application/json' => Response::FORMAT_JSON];
+        return $behaviors;
     }
     /*public function actions()
     {
