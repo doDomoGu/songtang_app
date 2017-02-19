981d600ef2605001cb48f5bb6afb618d1d0a6d46
doDomoGu <kaka_71936410@qq.com>
2017-02-19 21:03:47 +0800

    OA 首页

--- oa/views/site/index.php
+++ oa/views/site/index.php
@@ -4,25 +4,28 @@ use yii\helpers\Html;
 $this->title = '';
 
 $categoryType = TaskCategory::getTypeList();
-
+oa\assets\AppAsset::addCssFile($this,'css/main/site/index.css');
 ?>
 
 
-<?php foreach($categoryType as $k=>$v):?>
-    <div class="task-category panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title"><?=$v?></h3>
+<?php $j=0;foreach($categoryType as $k=>$v):?>
+    <div class="task-category <?=$j==0?'task-first':''?>" >
+        <div class="category-heading">
+            <h3 class="category-title"><?=$v?></h3>
         </div>
-        <div class="panel-body">
+        <div class="category-list">
             <?php $list = TaskCategory::find()->where(['type'=>$k,'show_flag'=>1,'status'=>1])->orderBy('ord asc')->all();?>
             <?php if(!empty($list)):?>
-            <ul class="list-unstyled">
-                <?php foreach($list as $l):?>
-                <li><?=$l->name?></li>
-                <?php endforeach;?>
-            </ul>
+                <?php $i=0;foreach($list as $l):?>
+                <li <?=$i%3==2?'class="li-3rd"':''?> ><?=$l->name?></li>
+                <?php $i++;endforeach;?>
+            <?php endif;?>
+            <?php if(count($list)%3>0):?>
+            <?php for($k=count($list)%3;$k<3;$k++):?>
+                <li <?=$k==2?'class="li-3rd"':''?> >&nbsp;</li>
+            <?php endfor;?>
             <?php endif;?>
         </div>
     </div>
 
-<?php endforeach;?>
+<?php $j++;endforeach;?>
