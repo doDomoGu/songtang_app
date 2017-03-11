56010312004d24719da3391be9e38c3b23264dbe
doDomoGu <kaka_71936410@qq.com>
2017-03-12 04:05:07 +0800

    OA 首页

--- oa/views/site/index.php
+++ oa/views/site/index.php
@@ -6,26 +6,17 @@ $this->title = '';
 $categoryType = TaskCategory::getTypeList();
 oa\assets\AppAsset::addCssFile($this,'css/main/site/index.css');
 ?>
-
-
-<?php $j=0;foreach($categoryType as $k=>$v):?>
-    <div class="task-category <?=$j==0?'task-first':''?>" >
-        <div class="category-heading">
-            <h3 class="category-title"><?=$v?></h3>
-        </div>
-        <div class="category-list">
+<section id="task-index">
+    <div class="task-content">
+        <?php $i=1;foreach($categoryType as $k=>$v):?>
+            <ul class="ul-<?=$i?>">
             <?php $list = TaskCategory::find()->where(['type'=>$k,'show_flag'=>1,'status'=>1])->orderBy('ord asc')->all();?>
             <?php if(!empty($list)):?>
-                <?php $i=0;foreach($list as $l):?>
-                <li <?=$i%3==2?'class="li-3rd"':''?> ><?=$l->name?></li>
-                <?php $i++;endforeach;?>
+                <?php foreach($list as $l):?>
+                <li><?=$l->name?></li>
+                <?php endforeach;?>
             <?php endif;?>
-            <?php if(count($list)%3>0):?>
-            <?php for($k=count($list)%3;$k<3;$k++):?>
-                <li <?=$k==2?'class="li-3rd"':''?> >&nbsp;</li>
-            <?php endfor;?>
-            <?php endif;?>
-        </div>
+            </ul>
+        <?php $i++;endforeach;?>
     </div>
-
-<?php $j++;endforeach;?>
+</section>
