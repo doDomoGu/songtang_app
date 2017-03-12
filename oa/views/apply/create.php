f54cd5ecd7fd515ae9b59df9ccf53348948ebc6f
doDomoGu <kaka_71936410@qq.com>
2017-03-12 23:43:24 +0800

    发起申请页面

--- oa/views/apply/create.php
+++ oa/views/apply/create.php
@@ -9,7 +9,10 @@ $this->title = '发起申请';
 
 <section class="panel panel-default">
     <div class="panel-heading">
-        <h3><?=Html::img('/images/main/apply/create-icon.png')?> &nbsp;&nbsp;&nbsp;<?=$this->title?></h3>
+        <h3>
+            <?=$this->title?>
+        </h3>
+        <?=Html::img('/images/main/apply/create-icon-2.png')?>
     </div>
     <div class="panel-body">
 
@@ -18,21 +21,21 @@ $this->title = '发起申请';
     'id' => 'apply-create-form',
     'options' => ['class' => 'form-horizontal','autocomplete'=>'off'],
     'fieldConfig' => [
-        'template' => "{label}\n<div class=\"col-lg-5\">{input}{hint}</div>\n<div class=\"col-lg-5\">{error}</div>",
-        'labelOptions' => ['class' => 'col-lg-2 control-label'],
+        'template' => "{label}\n<div class=\"col-lg-7\">{input}{hint}</div>\n<div class=\"col-lg-2\">{error}</div>",
+        'labelOptions' => ['class' => 'col-lg-3 control-label'],
     ],
 ]); ?>
-<?= $form->field($model, 'title') ?>
+<?= $form->field($model, 'title')->label(Html::img('/images/main/apply/create-head.png').'&nbsp;&nbsp;'.$model->attributeLabels()['title'],['style'=>'text-align:left;padding-left:60px;']) ?>
 
 <?= $form->field($model, 'task_id',[
-    'template'=>"{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-7\">{error}</div><div class=\"col-lg-offset-2 col-lg-10 task-preview \"></div>"
-])->dropDownList($tasks,['prompt'=>'==请选择==','class'=>['form-control task-select']])  ?>
+    'template'=>"{label}\n<div class=\"col-lg-7\">{input}</div>\n<div class=\"col-lg-2\">{error}</div><div class=\"col-lg-offset-3 col-lg-9 task-preview \"></div>"
+])->dropDownList($tasks,['prompt'=>'==请选择==','class'=>['form-control task-select']])->label(Html::img('/images/main/apply/create-head-2.png').'&nbsp;&nbsp;'.$model->attributeLabels()['task_id'],['style'=>'text-align:left;padding-left:60px;'])  ?>
 
 
-<?= $form->field($model, 'message')->textarea(['rows'=>10]) ?>
+<?= $form->field($model, 'message')->textarea(['rows'=>10])->label(Html::img('/images/main/apply/create-head-3.png').'&nbsp;&nbsp;'.$model->attributeLabels()['message'],['style'=>'text-align:left;padding-left:60px;'])   ?>
 
 <div class="form-group">
-    <div class="col-lg-offset-2 col-lg-10">
+    <div class="col-lg-offset-3 col-lg-9">
         <?= Html::submitButton('提交', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
     </div>
 </div>
