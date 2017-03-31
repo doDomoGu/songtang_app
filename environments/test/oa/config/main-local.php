8c73d20e0887a52bfe1ba01af85c4101b3760249
doDomoGu <kaka_71936410@qq.com>
2017-04-01 06:01:05 +0800

    测试环境 debug

--- environments/test/oa/config/main-local.php
+++ environments/test/oa/config/main-local.php
@@ -1,5 +1,6 @@
 <?php
-return [
+
+$config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -7,3 +8,19 @@ return [
         ],
     ],
 ];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+        'allowedIPs' => ['*.*.*.*','127.0.0.1', '::1'],
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
