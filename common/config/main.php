5e880449853ca16f7a239a4ba0dac4c0732ca6df
doDomoGu <kaka_71936410@qq.com>
2017-03-31 17:27:33 +0800

    测试 redis

--- common/config/main.php
+++ common/config/main.php
@@ -4,13 +4,14 @@ return [
     'components' => [
         'cache' => [
             //'class' => 'yii\caching\FileCache',
+            //'cachePath' => '@common/runtime/cache',
             'class' => 'yii\redis\Cache',
         ],
         'redis' => [
             'class' => 'yii\redis\Connection',
-            //'hostname' => 'localhost',
-            'hostname' => 'r-bp1520b0b452fd84.redis.rds.aliyuncs.com',
-            'password'=> 'Gljxyt110909',
+            'hostname' => 'localhost',
+            //'hostname' => '115.29.167.9',
+            //'password'=> 'r-bp1520b0b452fd84.redis.rds.aliyuncs.com:Gljxyt110909',
             'port' => 6379,
             'database' => 0,
         ],
