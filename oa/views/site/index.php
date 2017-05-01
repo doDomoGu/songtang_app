51d4f9de13ee191f0c6cd9c2d8eba1f19889b35c
doDomoGu <kaka_71936410@qq.com>
2017-05-01 23:18:22 +0800

    OA 首页

--- oa/views/site/index.php
+++ oa/views/site/index.php
@@ -14,14 +14,28 @@ oa\assets\AppAsset::addCssFile($this,'css/main/site/index.css');
                 <span class="task-title task-title-<?=$i?>">
                     <img src="/images/main/site/index_tag_<?=$i?>.png" />
                 </span>
-                <ul class="task-category-list ul-<?=$i?>">
+                <div class="task-category-list ul-<?=$i?>">
                     <?php $list = TaskCategory::find()->where(['type'=>$k,'show_flag'=>1,'status'=>1])->orderBy('ord asc')->all();?>
                     <?php if(!empty($list)):?>
+                        <?php if($k==1):?>
+                            <?php $list1 = array_slice($list,0,6);$list2 = array_slice($list,6,6);?>
+                            <ul>
+                            <?php foreach($list1 as $l):?>
+                                <li><?=$l->name?></li>
+                            <?php endforeach;?>
+                            </ul>
+                            <ul>
+                            <?php foreach($list2 as $l):?>
+                                <li><?=$l->name?></li>
+                            <?php endforeach;?>
+                            </ul>
+                        <?php else:?>
                         <?php foreach($list as $l):?>
                             <li><?=$l->name?></li>
                         <?php endforeach;?>
+                        <?php endif;?>
                     <?php endif;?>
-                </ul>
+                </div>
             </div>
 
         <?php $i++;endforeach;?>
