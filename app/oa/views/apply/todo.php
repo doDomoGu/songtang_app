6c2752de8eeb03c6ebd0cceb59159779f1df2a97
doDomoGu <kaka_71936410@qq.com>
2017-05-31 00:54:31 +0800

    OA  前台 列表

--- app/oa/views/apply/todo.php
+++ app/oa/views/apply/todo.php
@@ -1,6 +1,7 @@
 <?php
 use yii\bootstrap\Modal;
 use yii\bootstrap\Html;
+use oa\models\Apply;
 
 $this->title = '待办事项';
 oa\assets\AppAsset::addJsFile($this,'js/main/apply/my.js');
@@ -21,6 +22,7 @@ oa\assets\AppAsset::addCssFile($this,'css/main/apply/todo.css');
                 <th><span><?=Html::img('/images/main/apply/th_1.png',['style'=>''])?> &nbsp;&nbsp; 标题</span></th>
                 <th><span><?=Html::img('/images/main/apply/th_2.png',['style'=>''])?> &nbsp;&nbsp; 任务表</span></th>
                 <th><span><?=Html::img('/images/main/apply/th_3.png',['style'=>''])?> &nbsp;&nbsp; 申请时间</span></th>
+                <th><span><?=Html::img('/images/main/apply/th_3.png',['style'=>''])?> &nbsp;&nbsp; 状态</span></th>
                 <th class="last"><span><?=Html::img('/images/main/apply/th_4.png',['style'=>''])?> &nbsp;&nbsp; 操作</span></th>
             </tr>
             <tbody>
@@ -30,6 +32,7 @@ oa\assets\AppAsset::addCssFile($this,'css/main/apply/todo.css');
                     <td><span><?=$l->title?></span></td>
                     <td><span><?=$l->task->title?></span></td>
                     <td><span><?=$l->add_time?></span></td>
+                    <td><span><?=Apply::getStatusCn($l->status)?></span></td>
                     <td class="last">
                         <span><?=Html::a('进行操作','/apply/operation?id='.$l->id,['class'=>'btn btn-success btn-xs'])?></span>
                     </td>
