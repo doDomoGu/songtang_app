85c2060b7f127ee62c1f6bb887ee2189ec3799ca
doDomoGu <kaka_71936410@qq.com>
2017-07-02 22:36:04 +0800

    没有权限页面 增加退出按钮

--- app/ucenter/views/site/no-auth.php
+++ app/ucenter/views/site/no-auth.php
@@ -1,1 +1,5 @@
 <h3>没有使用权限！</h3>
+
+
+<?php 
+echo  \yii\bootstrap\Html::a('退出',Yii::$app->params['loginUrl'].'/site/logout');
--- app/yun/views/site/no_auth.php
+++ app/yun/views/site/no_auth.php
@@ -1,3 +1,5 @@
 <?php
 
 echo '没有权限';
+
+echo  \yii\bootstrap\Html::a('退出',Yii::$app->params['loginUrl'].'/site/logout');
