fba700d4eb0b60faaab72e16ce62d5dd8a766ea4
doDomoGu <kaka_71936410@qq.com>
2017-07-14 14:26:05 +0800

    OA 首页 样式

--- app/oa/views/site/index.php
+++ app/oa/views/site/index.php
@@ -21,8 +21,8 @@ oa\assets\AppAsset::addCssFile($this,'css/main/site/index.css');
                     <li><a href="/apply/create?category=8">异动</a></li>
                     <li><a href="/apply/create?category=25">请假</a></li>
                     <li class="last-line"><a href="/apply/create?category=11">加班</a></li>
-                    <li class="last-line word-line-2"><a href="/apply/create?category=10">外出出差</a></li>
-                    <li class="last-line word-line-3"><a href="/apply/create?category=26">劳动合同续签</a></li>
+                    <li class="last-line word-line-2"><a href="/apply/create?category=10">外出<br/>出差</a></li>
+                    <li class="last-line word-line-3"><a href="/apply/create?category=26">劳动<br/>合同<br/>续签</a></li>
                 </ul>
             </div>
         </div>
@@ -32,10 +32,10 @@ oa\assets\AppAsset::addCssFile($this,'css/main/site/index.css');
             </span>
             <div class="task-category-list ul-2">
                 <ul>
-                    <li class="word-line-2"><a href="/apply/create?category=12">日常报销</a></li>
-                    <li class="word-line-2"><a href="/apply/create?category=13">差旅报销</a></li>
-                    <li class="word-line-2"><a href="/apply/create?category=14">对公付款</a></li>
-                    <li class="word-line-2"><a href="/apply/create?category=15">开票申请</a></li>
+                    <li class="word-line-2"><a href="/apply/create?category=12">日常<br/>报销</a></li>
+                    <li class="word-line-2"><a href="/apply/create?category=13">差旅<br/>报销</a></li>
+                    <li class="word-line-2"><a href="/apply/create?category=14">对公<br/>付款</a></li>
+                    <li class="word-line-2"><a href="/apply/create?category=15">开票<br/>申请</a></li>
                 </ul>
             </div>
         </div>
@@ -49,8 +49,8 @@ oa\assets\AppAsset::addCssFile($this,'css/main/site/index.css');
                     <li class="word-num-3"><a href="/apply/create?category=19">法人章</a></li>
                     <li class="word-num-3"><a href="/apply/create?category=27">合同章</a></li>
                     <li><a href="/apply/create?category=18">证照</a></li>
-                    <li class="word-line-2 last-line"><a href="/apply/create?category=28">印章制作</a></li>
-                    <li class="word-line-2 last-line"><a href="/apply/create?category=29">印章异常</a></li>
+                    <li class="word-line-2 last-line"><a href="/apply/create?category=28">印章<br/>制作</a></li>
+                    <li class="word-line-2 last-line"><a href="/apply/create?category=29">印章<br/>异常</a></li>
                 </ul>
             </div>
         </div>
@@ -61,8 +61,8 @@ oa\assets\AppAsset::addCssFile($this,'css/main/site/index.css');
             <div class="task-category-list ul-4">
                 <ul>
                     <li><a href="/apply/create?category=20">采购</a></li>
-                    <li class="word-line-2"><a href="/apply/create?category=22">名片胸牌</a></li>
-                    <li class="last-line word-line-2"><a href="/apply/create?category=24">事项审批</a></li>
+                    <li class="word-line-2"><a href="/apply/create?category=22">名片<br/>胸牌</a></li>
+                    <li class="last-line word-line-2"><a href="/apply/create?category=24">事项<br/>审批</a></li>
                 </ul>
             </div>
         </div>
